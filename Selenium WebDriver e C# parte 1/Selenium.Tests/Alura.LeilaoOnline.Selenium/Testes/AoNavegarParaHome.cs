364a15a203d6e4ceca0b085a4ff92545808d7824
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-07-13 17:25:43 -0300

    Aprendendo mais sobre o Selenium

---
+++ Selenium WebDriver e C# parte 1/Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoNavegarParaHome.cs
@@ -0,0 +1,51 @@
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoNavegarParaHome 
+    {
+        private IWebDriver driver;
+
+        //Setup
+        public AoNavegarParaHome(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarLeiloesNoTitulo()
+        {
+            //Arrange
+            
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:5001");
+
+            //Assert
+            Assert.Contains("Leilões", driver.Title);
+        }
+
+        [Fact]
+        public void DadoChromeAbertoDeveMostrarProximosLeiloesNaPagina()
+        {
+            //Arrange
+
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:5001");
+
+            //Assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+
+        
+    }
+}
