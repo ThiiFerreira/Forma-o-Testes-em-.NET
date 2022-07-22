0869606009a8a862b598d86da4a7fc1425748015
Thiago da Silva Ferreira <thiago.ferreira.p2001@gmail.com>
2022-07-22 12:42:24 -0300

    utilizanso Interactions do selenium para lidar com elementos que só irão aparecer depois de determinada ação

---
+++ Selenium WebDriver e C# parte 2/Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using OpenQA.Selenium;
+using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+
+namespace Alura.LeilaoOnline.Selenium.Testes
+{
+    [Collection("Chrome Driver")]
+    public class AoEfetuarRegistro
+    {
+        private IWebDriver driver;
+
+        public AoEfetuarRegistro(TestFixture fixture)
+        {
+            driver = fixture.Driver;
+        }
+        
+        [Fact]
+        public void DadoInformacoesValidasDeveIrParaPaginaDEAgradecimento()
+        {
+            //Arrange - dado chrome aberto, pagina inicial do sist de leilões,
+            //dados de registros validos informados
+            var registroPO = new RegistroPO(driver);
+            registroPO.Visitar();
+
+            registroPO.PreencherFormulario("Daniel Portugal", "daniel.portugal@caelum.com.br", "123", "123");
+
+            //Act - efetuo registro
+            registroPO.SubmeteFormulario();
+
+            //Assert - devo ser direcionado para uma pagina de agradecimento
+            Assert.Contains("Obrigado", driver.PageSource);
+        }
+
+        [Theory]
+        [InlineData("","daniel.portugal@caelum.com.br","123","123")]
+        [InlineData("Daniel Portugal","daniel","123","123")]
+        [InlineData("Daniel Portugal","daniel.portugal@caelum.com.br","123","456")]
+        [InlineData("Daniel Portugal", "daniel.portugal@caelum.com.br","123","")]
+        public void DadoInformacoesInvalidasDeveContinuarNaHome(
+            string nome,
+            string email,
+            string senha,
+            string confirmaSenha)
+        {
+            //Arrange - dado chrome aberto, pagina inicial do sist de leilões,
+            //dados de registros validos informados
+            var registroPO = new RegistroPO(driver);
+            registroPO.Visitar();
+
+
+            registroPO.PreencherFormulario(nome, email, senha, confirmaSenha);
+
+            //Act - efetuo registro
+            registroPO.SubmeteFormulario();
+
+            //Assert - devo ser direcionado para uma pagina de agradecimento
+            Assert.Contains("section-registro", driver.PageSource);
+        }
+
+        [Fact]
+        public void DadoNomeEmBrancoDeveMostrarMensagemDeErro()
+        {
+            //Arrange
+            var registroPO = new RegistroPO(driver);
+
+            registroPO.Visitar();
+
+            //Act
+            registroPO.SubmeteFormulario();
+
+            //Assert
+            IWebElement elemento = driver.FindElement(By.CssSelector("span.msg-erro[data-valmsg-for=Nome]"));
+            Assert.Equal("The Nome field is required.", elemento.Text);
+        }
+
+        [Fact]
+        public void DadoEmailInvalidoDeveMostrarMensagemDeErro()
+        {
+            //Arrange
+            var registroPO = new RegistroPO(driver);
+            registroPO.Visitar();
+
+            registroPO.PreencherFormulario(
+                nome: "",
+                email: "daniel",
+                senha: "",
+                confirmSenha: ""
+                );
+            
+            //Act
+            registroPO.SubmeteFormulario();
+
+            //Assert
+            Assert.Equal("Please enter a valid email address.", registroPO.EmailMensagemErro);
+        }
+    }
+}
